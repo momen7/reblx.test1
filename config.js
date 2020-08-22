54c6381be38eb0050f19814568e0d67148b115b9
REBEL.-X <45073137+momen7@users.noreply.github.com>
2020-08-22 21:08:37 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  TOKEN: "Njk3MDgyOTIxMDM0NTc5OTc5.XoyHFA.pl-mBNPzjUkzoD3FZXS5gzgR04U", //تعديل مهم توكن بوتك
+  YT_API_KEY: "api ",
+  prefix: "=",
+  devs: ["427559563378556929"] ///تعديل مهم غير هذا الرقم بـ ايدي حسابك
+};
